794759721a6a96489f50f8c8f3084de5905fd2de
algochemy <152131529+algochemy@users.noreply.github.com>
2024-04-30 11:15:33 +0900

    feat: info_screen 작성

--- lib/presentation/screen/info_screen.dart
+++ lib/presentation/screen/info_screen.dart
@@ -1,51 +1,129 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_speak_talk/presentation/screen/login_screen.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'login_screen.dart';
 
-class InfoScreen extends StatefulWidget {
+class InfoScreen extends StatelessWidget {
   const InfoScreen({super.key});
 
   @override
-  State<InfoScreen> createState() => _InfoScreenState();
-}
-
-class _InfoScreenState extends State<InfoScreen> {
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Container(
-          child: TextButton(
-            onPressed: () async {
-              final prefs = await SharedPreferences.getInstance();
-              // auth_utils 파일에 isFirstLogin 값을 true로 설정.
-              //첫 번째 로그인 상태로
-              await prefs.setBool('isFirstLogin', true);
-
-              // 로그아웃 후에 로그아웃된거 메시지
-              ScaffoldMessenger.of(context).showSnackBar(
-                const SnackBar(
-                  content: Text("로그아웃 되었습니다."),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Column(
+              children: [
+                const SizedBox(
+                  height: 80,
                 ),
-              );
 
-              // // 로그아웃 담에 로그인 페이지로
-              Navigator.pushReplacement(
-                context,
-                MaterialPageRoute(builder: (context) => const Login()),
-              );
-            },
-            style: TextButton.styleFrom(
-              backgroundColor: Colors.amber,
-              minimumSize: const Size(75, 125),
+                Row(
+                  children: [
+                    const CircleAvatar(
+                      radius: 75,
+                    ), // TODO 사진 업로드 기능 추가
+                    const SizedBox(
+                      width: 20,
+                    ),
+                    _buildUserInfoSection(context), // 유저 이름 섹션 불러오기
+                  ],
+                ),
+
+                const SizedBox(
+                  height: 20,
+                ),
+
+                ListView.separated(
+                  shrinkWrap: true, // ListView의 높이를 감싸는 콘텐츠에 맞춥니다.
+                  itemCount: 3, // 항목 개수
+                  separatorBuilder: (context, index) => const Divider(), // 구분선 추가
+                  itemBuilder: (context, index) {
+                    final title = ['요금제 업그레이드', '개인정보처리 약관', '로그아웃'][index];
+                    return InkWell(
+                      onTap: () {
+                        if (index == 2) {
+                          _logout(context); // 로그아웃 함수 호출
+                        } else {
+                          // 요금제 업그레이드, 개인정보처리 약관 등 클릭 시 처리
+                          // ... 각 항목에 해당하는 처리 코드 작성
+                        }
+                      },
+                      child: Container(
+                        decoration: const BoxDecoration(
+                          border: Border(
+                            left: BorderSide(width: 1.0), // 왼쪽 테두리 추가
+                            bottom: BorderSide(width: 1.0), // 아래쪽 테두리 추가
+                          ),
+                        ),
+                        padding: const EdgeInsets.all(16.0), // 여백 추가
+                        child: Row(
+                          children: [
+                            const Icon(Icons.arrow_right), // 화살표 아이콘 추가
+                            const SizedBox(width: 16.0),
+                            Text(
+                              title,
+                              style: const TextStyle(fontSize: 18),
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ],
             ),
-            child: const Text(
-              'Logout',
-              style: TextStyle(color: Colors.white),
-            ),
-          ),
+          ],
         ),
       ),
+    );
+  }
+
+  Widget _buildUserInfoSection(BuildContext context) {
+    return FutureBuilder<String>(
+      future: _getUserName(), // 이름 가져오는 FutureBuilder
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          final name = snapshot.data!;
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                '이름 : $name',
+                style: const TextStyle(fontSize: 22),
+              ),
+              const Text(
+                '가입일 : 2024-04-29',
+                style: TextStyle(fontSize: 18),
+              ),
+            ],
+          );
+        } else {
+          return const CircularProgressIndicator(); // 이름 로딩 중 표시
+        }
+      },
+    );
+  }
+
+  Future<String> _getUserName() async {
+    final prefs = await SharedPreferences.getInstance();
+    final name = prefs.getString('name') ?? 'Unknown';
+    return name;
+  }
+
+  void _logout(BuildContext context) async {
+    final prefs = await SharedPreferences.getInstance(); // 1. SharedPreferences 인스턴스 가져오기
+    await prefs.setBool('isFirstLogin', true); // 2. 'isFirstLogin' 값을 true로 설정하여 첫 번째 로그인 상태로 변경
+    // 3. 로그아웃 메시지 표시
+    ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(
+        content: Text("로그아웃 되었습니다."),
+      ),
+    );
+    // 4. 로그인 페이지로 이동 (Navigator.pushReplacement 사용하여 기존 페이지 스택 유지 안 함)
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (context) => const Login()),
     );
   }
 }
