e44e4d53bd67e4ae1b9c679dceef6fbbdb2cb9b0
algochemy <152131529+algochemy@users.noreply.github.com>
2024-05-13 15:26:50 +0900

    feat: Firebase 쿼리 최적화

--- lib/data/repository/weekely_message_counter_impl.dart
+++ lib/data/repository/weekely_message_counter_impl.dart
@@ -7,37 +7,33 @@ class WeeklyMessageCounterImpl implements WeeklyMessageCounter {
   @override
   Future<List<int>> getWeeklyMessageCount(String uid) async {
     try {
-      // 현재 날짜의 시작 시간 (12:00 AM)
       DateTime now = DateTime.now();
       DateTime startOfWeek = now.subtract(Duration(days: now.weekday - 1)); // 이번 주의 첫째 날 (월요일)
       DateTime endOfWeek = startOfWeek.add(Duration(days: 7)); // 이번 주의 마지막 날 (다음 주의 월요일)
 
-      // 파이어베이스에서 해당 uid를 가진 모든 문서 가져오기
-      QuerySnapshot querySnapshot = await _conversationCollection.where('uid', isEqualTo: uid).get();
-
-      // 요일별 발화 개수를 저장할 리스트 초기화
+      QuerySnapshot querySnapshot = await _conversationCollection
+          .where('uid', isEqualTo: uid)
+          .where('startTime', isGreaterThanOrEqualTo: startOfWeek)
+          .where('startTime', isLessThan: endOfWeek)
+          .get();
+      
       List<int> messageCounts = List.filled(7, 0);
 
-      // 모든 문서에 대해 처리
       querySnapshot.docs.forEach((conversationDoc) {
         List<dynamic>? messages = (conversationDoc.data() as Map<String, dynamic>?)?['messages'];
 
-        // 메시지 확인 및 발화 개수 계산
         messages?.forEach((message) {
-          DateTime? timestamp = message['timestamp'] != null ? DateTime.parse(message['timestamp']) : null;
-          if (timestamp != null && timestamp.isAfter(startOfWeek) && timestamp.isBefore(endOfWeek)) {
-            int dayOfWeek = (timestamp.weekday + 5) % 7; // 시작 요일을 월요일로 설정하기 위해 보정
-            if (message['isMe'] == true) {
-              // isMe가 true이면 발화인 것으로 간주하여 누적
-              messageCounts[dayOfWeek]++;
-            }
+          if (message['isMe'] == true) {
+            DateTime timestamp = DateTime.parse(message['timestamp']);
+            int dayOfWeek = timestamp.weekday - 1; // 일요일부터 0으로 시작하도록 조정
+            messageCounts[dayOfWeek]++;
           }
         });
       });
 
       return messageCounts;
     } catch (e) {
-      throw Exception('Failed to get message count by week: $e');
+      throw Exception('Failed to get weekly message count: $e');
     }
   }
 }
