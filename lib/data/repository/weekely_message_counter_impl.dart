b0b6410ab6b99635ccc6ff45050a50d208e41bd3
algochemy <152131529+algochemy@users.noreply.github.com>
2024-05-14 12:20:46 +0900

    feat: 주간 성취도 그래프 기능 구현

--- lib/data/repository/weekely_message_counter_impl.dart
+++ lib/data/repository/weekely_message_counter_impl.dart
@@ -8,24 +8,25 @@ class WeeklyMessageCounterImpl implements WeeklyMessageCounter {
   Future<List<int>> getWeeklyMessageCount(String uid) async {
     try {
       DateTime now = DateTime.now();
-      DateTime startOfWeek = now.subtract(Duration(days: now.weekday - 1)); // 이번 주의 첫째 날 (월요일)
-      DateTime endOfWeek = startOfWeek.add(Duration(days: 7)); // 이번 주의 마지막 날 (다음 주의 월요일)
+      DateTime thisMonday = now.subtract(Duration(days: now.weekday - 1)); // 이번 주의 첫째 날 (월요일)
+      DateTime nextMonday = thisMonday.add(const Duration(days: 7)); // 다음 주의 월요일
 
       QuerySnapshot querySnapshot = await _conversationCollection
           .where('uid', isEqualTo: uid)
-          .where('startTime', isGreaterThanOrEqualTo: startOfWeek)
-          .where('startTime', isLessThan: endOfWeek)
+          .orderBy('startTime', descending: true) // startTime을 기준으로 내림차순 정렬
+          .where('startTime', isGreaterThanOrEqualTo: thisMonday)
+          .where('startTime', isLessThan: nextMonday)
           .get();
-      
+
       List<int> messageCounts = List.filled(7, 0);
 
       querySnapshot.docs.forEach((conversationDoc) {
-        List<dynamic>? messages = (conversationDoc.data() as Map<String, dynamic>?)?['messages'];
+        List<dynamic>? messages = (conversationDoc.data() as Map<String, dynamic>?)?['messages'] as List<dynamic>?;
 
         messages?.forEach((message) {
           if (message['isMe'] == true) {
-            DateTime timestamp = DateTime.parse(message['timestamp']);
-            int dayOfWeek = timestamp.weekday - 1; // 일요일부터 0으로 시작하도록 조정
+            Timestamp timestamp = message['timestamp']; // Timestamp 객체로 받음
+            int dayOfWeek = timestamp.toDate().weekday % 7; // 월요일부터 0으로 시작하도록 조정
             messageCounts[dayOfWeek]++;
           }
         });
--- lib/presentation/screen/home_screen.dart
+++ lib/presentation/screen/home_screen.dart
@@ -1,11 +1,12 @@
-import 'package:back_button_interceptor/back_button_interceptor.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:fl_chart/fl_chart.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_speak_talk/presentation/screen/info_pay_screen.dart';
 import 'package:flutter_speak_talk/presentation/screen/select_theme_screen.dart';
 import 'package:flutter_speak_talk/utils/firebase_store.dart';
+import '../../data/repository/weekely_message_counter_impl.dart';
 import '../../domain/model/profile.dart';
+import '../../domain/repository/weekely_message_counter.dart';
+import '../components/achieve_bar_chart.dart';
 import 'info_screen.dart';
 import 'talk_archive_screen.dart';
 
@@ -18,50 +19,16 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   final FirebaseStoreService _firebaseStoreService = FirebaseStoreService();
-
-  List<Profile> profiles = [];
+  final WeeklyMessageCounter _weeklyMessageCounter = WeeklyMessageCounterImpl();
 
   int _selectedIndex = 0;
   late List<Widget> _widgetOptions;
-
-  @override
-  void dispose() {
-    BackButtonInterceptor.remove(myInterceptor);
-    super.dispose();
-  }
-
-  bool myInterceptor(bool stopDefaultButtonEvent, RouteInfo info) {
-    _showExitConfirmationDialog();
-    return true;
-  }
-
-  void _showExitConfirmationDialog() {
-    showDialog(
-      context: context,
-      builder: (context) => AlertDialog(
-        title: const Text('앱 종료'),
-        content: const Text('앱을 종료하시겠습니까?'),
-        actions: <Widget>[
-          TextButton(
-            onPressed: () {
-              Navigator.of(context).pop();
-            },
-            child: const Text('x'),
-          ),
-          TextButton(
-            onPressed: () {
-              SystemNavigator.pop();
-            },
-            child: const Text('o'),
-          ),
-        ],
-      ),
-    );
-  }
+  List<int> _messageCounts = List.filled(7, 0);
 
   @override
   void initState() {
     super.initState();
+    _loadMessageCounts();
     _widgetOptions = [
       buildHomeTab(),
       SelectThemeScreen(
@@ -72,13 +39,208 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
       InfoScreen(),
     ];
-    BackButtonInterceptor.add(myInterceptor);
+  }
+
+  Future<void> _loadMessageCounts() async {
+    try {
+      // Firebase Authentication에서 현재 로그인된 사용자의 정보 가져오기
+      User? user = FirebaseAuth.instance.currentUser;
+      if (user != null) {
+        // WeeklyMessageCounterImpl에서 주간 메시지 수 가져오기
+        final List<int> counts = await _weeklyMessageCounter.getWeeklyMessageCount(user.uid);
+        setState(() {
+          _messageCounts = counts;
+        });
+      }
+    } catch (e) {
+      // Error handling
+      print('Failed to load message counts: $e');
+    }
   }
 
   void _onItemTapped(int index) {
     setState(() {
       _selectedIndex = index;
     });
+  }
+
+  Widget buildHomeTab() {
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false, // 뒤로가기 버튼 제거
+        backgroundColor: const Color(0xFFC4E6F3),
+        title: const Text('Home'),
+        actions: [
+          ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const InfoPayScreen(),
+                ),
+              );
+            },
+            style: ElevatedButton.styleFrom(backgroundColor: Colors.blue),
+            child: const Row(
+              children: [
+                Text('프리미엄 구독하기', style: TextStyle(color: Colors.white)),
+                Icon(
+                  Icons.attach_money,
+                  color: Colors.white,
+                  size: 15,
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: FutureBuilder<Profile>(
+          future: _firebaseStoreService.readProfile(),
+          builder: (BuildContext context, AsyncSnapshot<Profile> snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const CircularProgressIndicator();
+            } else if (snapshot.hasError) {
+              return Text('Error: ${snapshot.error}'); // 에러 메시지 출력
+            } else if (snapshot.hasData && snapshot.data != null) {
+              return buildHomeTabBody(snapshot.data!);
+            } else {
+              return const Text('No profile found');
+            }
+          },
+        ),
+      ),
+    );
+  }
+
+  Widget buildHomeTabBody(Profile profile) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.bottomLeft,
+          end: Alignment.topRight,
+          colors: [
+            Colors.black,
+            Colors.lightBlue,
+          ],
+        ),
+      ),
+      child: Column(
+        children: [
+          const SizedBox(height: 25),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                backgroundImage: NetworkImage(profile.profilePicture ?? ''),
+                radius: 50,
+                backgroundColor: Colors.red,
+              ),
+              const SizedBox(width: 20),
+              Column(
+                children: [
+                  Text(
+                    '이름: ${profile.name}',
+                    style: const TextStyle(fontSize: 18, color: Colors.white),
+                  ),
+                  Text(
+                    '${profile.membershipLevel} 회원 입니다',
+                    style: const TextStyle(fontSize: 18, color: Colors.white),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          const SizedBox(height: 25),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              const SizedBox(width: 30),
+              SizedBox(
+                width: 200,
+                height: 25,
+                child: Text(
+                  'AI 채팅 잔여횟수 (${profile.remainingChats} / 5)',
+                  style: const TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 10),
+          SizedBox(
+            width: 350,
+            height: 10,
+            child: LinearProgressIndicator(
+              value: profile.remainingChats / 5,
+              backgroundColor: Colors.black,
+              valueColor: const AlwaysStoppedAnimation<Color>(Colors.amber),
+            ),
+          ),
+          const SizedBox(height: 50),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Column(
+                children: [
+                  Text(
+                    '${profile.level} 레벨',
+                    style: const TextStyle(fontSize: 18, color: Colors.white),
+                  ),
+                  const Image(
+                    width: 30,
+                    height: 30,
+                    image: AssetImage('assets/bronze.png'),
+                  ),
+                ],
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const TalkArchiveScreen(),
+                    ),
+                  );
+                },
+                child: const Text(
+                  '지난 학습 보기',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.blueAccent,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 50),
+          const Padding(
+            padding: EdgeInsets.only(left: 25),
+            child: SizedBox(
+              width: 100,
+              height: 25,
+              child: Text(
+                '주간 성취도',
+                style: TextStyle(
+                  fontSize: 15,
+                  fontWeight: FontWeight.w600,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(height: 10),
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: AchieveBarChart(messageCounts: _messageCounts),
+          ),
+          const SizedBox(height: 100),
+        ],
+      ),
+    );
   }
 
   @override
@@ -107,241 +269,6 @@ class _HomeScreenState extends State<HomeScreen> {
         selectedItemColor: Colors.indigo,
         unselectedItemColor: Colors.grey,
         onTap: _onItemTapped,
-      ),
-    );
-  }
-
-  Widget buildHomeTab() {
-    return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: false, // 뒤로가기 버튼 제거
-        backgroundColor: const Color(0xFFC4E6F3),
-        title: const Text('Home'),
-        actions: [
-          ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => const InfoPayScreen()),
-                );
-              },
-              style: ElevatedButton.styleFrom(backgroundColor: Colors.blue),
-              child: const Row(
-                children: [
-                  Text('프리미엄 구독하기', style: TextStyle(color: Colors.white)),
-                  Icon(
-                    Icons.attach_money,
-                    color: Colors.white,
-                    size: 15,
-                  ),
-                ],
-              )),
-        ],
-      ),
-      body: SingleChildScrollView(
-        // FutureBuilder 비동기 처리
-        child: FutureBuilder<Profile>(
-          future: _firebaseStoreService.readProfile(),
-          builder: (BuildContext context, AsyncSnapshot<Profile> snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const CircularProgressIndicator();
-            } else if (snapshot.hasError) {
-              return Text('Error: ${snapshot.error}'); // 에러 메시지 출력
-            } else if (snapshot.hasData && snapshot.data != null) {
-              return Container(
-                decoration: const BoxDecoration(
-                  gradient: LinearGradient(
-                    begin: Alignment.bottomLeft,
-                    end: Alignment.topRight,
-                    colors: [
-                      Colors.black,
-                      Colors.lightBlue,
-                    ],
-                  ),
-                ),
-                child: Column(
-                  children: [
-                    const SizedBox(height: 25),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        CircleAvatar(
-                          backgroundImage:
-                              NetworkImage(snapshot.data!.profilePicture ?? ''),
-                          radius: 50,
-                          backgroundColor: Colors.red,
-                        ),
-                        SizedBox(width: 20),
-                        Column(
-                          children: [
-                            Text(
-                              '이름: ${snapshot.data!.name}',
-                              style:
-                                  TextStyle(fontSize: 18, color: Colors.white),
-                            ),
-                            Text(
-                              // 멤버쉽(회원) 레벨
-                              '${snapshot.data!.membershipLevel} 회원 입니다',
-                              style:
-                                  TextStyle(fontSize: 18, color: Colors.white),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                    const SizedBox(height: 25),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        SizedBox(width: 30),
-                        SizedBox(
-                          width: 200,
-                          height: 25,
-                          child: Text(
-                            'AI 채팅 잔여횟수 (${snapshot.data!.remainingChats} / 5)',
-                            style: TextStyle(
-                              fontSize: 15,
-                              fontWeight: FontWeight.w600,
-                              color: Colors.white,
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                    const SizedBox(height: 10),
-                    SizedBox(
-                      width: 350,
-                      height: 10,
-                      child: LinearProgressIndicator(
-                        value: snapshot.data!.remainingChats / 5,
-                        backgroundColor: Colors.black,
-                        valueColor: AlwaysStoppedAnimation<Color>(Colors.amber),
-                      ),
-                    ),
-                    const SizedBox(height: 50),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        Column(
-                          children: [
-                            Text(
-                              '${snapshot.data!.level} 레벨',
-                              style:
-                                  TextStyle(fontSize: 18, color: Colors.white),
-                            ),
-                            Image(
-                              width: 30,
-                              height: 30,
-                              image: AssetImage('assets/bronze.png'),
-                            ),
-                          ],
-                        ),
-                        ElevatedButton(
-                          onPressed: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) =>
-                                      const TalkArchiveScreen()), // Create a MaterialPageRoute to the TalkArchiveScreen
-                            );
-                          },
-                          child: const Text(
-                            '지난 학습 보기',
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                color: Colors.blueAccent),
-                          ),
-                        ),
-                      ],
-                    ),
-                    const SizedBox(height: 50),
-                    const Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        SizedBox(width: 25),
-                        SizedBox(
-                          width: 100,
-                          height: 25,
-                          child: Text(
-                            '주간 성취도',
-                            style: TextStyle(
-                              fontSize: 15,
-                              fontWeight: FontWeight.w600,
-                              color: Colors.white,
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                    const SizedBox(height: 10),
-                    Container(
-                      width: 350,
-                      height: 200,
-                      color: Colors.blue,
-                      child: BarChart(
-                        BarChartData(
-                          alignment: BarChartAlignment.spaceAround,
-                          maxY: 9,
-                          barTouchData: BarTouchData(enabled: false),
-                          titlesData: FlTitlesData(
-                            show: true,
-                            rightTitles: const AxisTitles(
-                                sideTitles: SideTitles(showTitles: false)),
-                            topTitles: const AxisTitles(
-                                sideTitles: SideTitles(showTitles: false)),
-                            bottomTitles: AxisTitles(
-                              sideTitles: SideTitles(
-                                showTitles: true,
-                                reservedSize: 30,
-                                getTitlesWidget:
-                                    (double value, TitleMeta meta) {
-                                  const texts = [
-                                    '일',
-                                    '월',
-                                    '화',
-                                    '수',
-                                    '목',
-                                    '금',
-                                    '토'
-                                  ];
-                                  return SideTitleWidget(
-                                    axisSide: meta.axisSide,
-                                    space: 4.0,
-                                    child: Text(texts[value.toInt()]),
-                                  );
-                                },
-                              ),
-                            ),
-                            leftTitles: const AxisTitles(
-                                sideTitles: SideTitles(showTitles: false)),
-                          ),
-                          gridData: const FlGridData(show: false),
-                          borderData: FlBorderData(show: false),
-                          barGroups: List.generate(
-                            7,
-                            (index) => BarChartGroupData(
-                              x: index,
-                              barRods: [
-                                BarChartRodData(
-                                  toY: index + 1,
-                                  color: Colors.amber,
-                                )
-                              ],
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                    const SizedBox(height: 100),
-                  ],
-                ),
-              );
-            } else {
-              return const Text('No profile found');
-            }
-          },
-        ),
       ),
     );
   }
